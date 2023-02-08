1474833637235c64d8f1f049f4dd732f0792f691
niknikolaou <nik.nikolaou@gmail.com>
2023-02-08 13:44:10 +0200

    Convert Multiwallet Connection

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
+import { useAddress, useMetamask, useDisconnect, ConnectWallet } from "@thirdweb-dev/react";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Home.module.css";
@@ -24,25 +24,7 @@ export default function Header() {
         </div>
       </div>
       <div className={styles.right}>
-        {address ? (
-          <>
-            <a
-              className={styles.secondaryButton}
-              onClick={() => disconnectWallet()}
-            >
-              Disconnect Wallet
-            </a>
-            <p style={{ marginLeft: 8, marginRight: 8, color: "grey" }}>|</p>
-            <p>{address.slice(0, 6).concat("...").concat(address.slice(-4))}</p>
-          </>
-        ) : (
-          <a
-            className={styles.mainButton}
-            onClick={() => connectWithMetamask()}
-          >
-            Connect Wallet
-          </a>
-        )}
+        <ConnectWallet/>
       </div>
     </div>
   );
